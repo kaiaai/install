90006f6e936f1bc36a374b44634f079bc1555a0e
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-07-26 18:38:12 -0700

    Launch bugfix

--- micro-ros-agent_humble-desktop/micro-ros_entrypoint.sh
+++ micro-ros-agent_humble-desktop/micro-ros_entrypoint.sh
@@ -1,17 +1,4 @@
-. "/opt/ros/$ROS_DISTRO/setup.sh"
-. "/uros_ws/install/local_setup.sh"
+#!/bin/bash
+set -e
 
-if [ "$MICROROS_DISABLE_SHM" = "1" ] ; then
-    if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
-        export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm_localhost_only.xml
-    else
-        export FASTRTPS_DEFAULT_PROFILES_FILE=/tmp/disable_fastdds_shm.xml
-    fi
-fi
-
-if [ $# -eq 0 ]; then
-  exec ros2 run micro_ros_agent micro_ros_agent "$@"
-else
-  set -e
-  exec "$@"
-fi
+exec "$@"
