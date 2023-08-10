9a1a292297e3a7650979ad84dbd806b41a93cf54
Ilia O <iliao@remake.ai>
2023-08-10 01:00:44 -0700

    Build tweak

--- build_all.cmd
+++ build_all.cmd
@@ -1,3 +1,8 @@
+cd ..\..\kaia-ros
+docker build -t kaiaai/kaia-ros:humble --build-arg distro_tag=humble .
+docker push kaiaai/kaia-ros:humble
+docker build -t kaiaai/kaia-ros:iron --build-arg distro_tag=iron .
+docker push kaiaai/kaia-ros:iron
 cd .\etc\micro-ros-base-desktop
 docker build -t kaiaai/micro-ros-base-desktop:humble --build-arg distro_tag=humble .
 docker push kaiaai/micro-ros-base-desktop:humble
@@ -8,11 +13,6 @@ docker build -t kaiaai/micro-ros-agent-desktop:humble --build-arg distro_tag=hum
 docker push kaiaai/micro-ros-agent-desktop:humble
 docker build -t kaiaai/micro-ros-agent-desktop:iron --build-arg distro_tag=iron .
 docker push kaiaai/micro-ros-agent-desktop:iron
-cd ..\..\kaia-ros
-docker build -t kaiaai/kaia-ros:humble --build-arg distro_tag=humble .
-docker push kaiaai/kaia-ros:humble
-docker build -t kaiaai/kaia-ros:iron --build-arg distro_tag=iron .
-docker push kaiaai/kaia-ros:iron
 cd ..\kaia-ros-dev
 docker build -t kaiaai/kaia-ros-dev:humble --build-arg distro_tag=humble .
 docker push kaiaai/kaia-ros-dev:humble
