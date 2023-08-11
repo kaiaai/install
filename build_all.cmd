508446563d3b00894861c547ad90ab88ca086312
Ilia O <iliao@remake.ai>
2023-08-11 02:14:59 -0700

    Build-all fix; cli alias; package env

--- build_all.cmd
+++ build_all.cmd
@@ -1,9 +1,9 @@
-cd ..\..\kaia-ros
+cd .\kaia-ros
 docker build -t kaiaai/kaia-ros:humble --build-arg distro_tag=humble .
 docker push kaiaai/kaia-ros:humble
 docker build -t kaiaai/kaia-ros:iron --build-arg distro_tag=iron .
 docker push kaiaai/kaia-ros:iron
-cd .\etc\micro-ros-base-desktop
+cd ..\etc\micro-ros-base-desktop
 docker build -t kaiaai/micro-ros-base-desktop:humble --build-arg distro_tag=humble .
 docker push kaiaai/micro-ros-base-desktop:humble
 docker build -t kaiaai/micro-ros-base-desktop:iron --build-arg distro_tag=iron .
