4ceb49683152f26b13db2a384fa2d72917def376
Ilia O <iliao@remake.ai>
2023-08-23 08:59:03 -0700

    Parallel push

---
+++ build/build_all_bg.cmd
@@ -0,0 +1,28 @@
+cd .\kaia-ros
+docker image rm kaiaai/kaia-ros:humble
+docker build --no-cache -t kaiaai/kaia-ros:humble --build-arg distro_tag=humble .
+START docker push kaiaai/kaia-ros:humble
+docker image rm kaiaai/kaia-ros:iron
+docker build --no-cache -t kaiaai/kaia-ros:iron --build-arg distro_tag=iron .
+START docker push kaiaai/kaia-ros:iron
+cd ..\etc\micro-ros-base-desktop
+docker image rm kaiaai/micro-ros-base-desktop:humble
+docker build --no-cache -t kaiaai/micro-ros-base-desktop:humble --build-arg distro_tag=humble .
+START docker push kaiaai/micro-ros-base-desktop:humble
+docker image rm kaiaai/micro-ros-base-desktop:iron
+docker build --no-cache -t kaiaai/micro-ros-base-desktop:iron --build-arg distro_tag=iron .
+START docker push kaiaai/micro-ros-base-desktop:iron
+cd ..\micro-ros-agent-desktop
+docker image rm kaiaai/micro-ros-agent-desktop:humble
+docker build --no-cache -t kaiaai/micro-ros-agent-desktop:humble --build-arg distro_tag=humble .
+START docker push kaiaai/micro-ros-agent-desktop:humble
+docker image rm kaiaai/micro-ros-agent-desktop:iron
+docker build --no-cache -t kaiaai/micro-ros-agent-desktop:iron --build-arg distro_tag=iron .
+START docker push kaiaai/micro-ros-agent-desktop:iron
+cd ..\..\kaia-ros-dev
+docker image rm kaiaai/kaia-ros-dev:humble
+docker build --no-cache -t kaiaai/kaia-ros-dev:humble --build-arg distro_tag=humble .
+START docker push kaiaai/kaia-ros-dev:humble
+docker image rm kaiaai/kaia-ros-dev:iron
+docker build --no-cache -t kaiaai/kaia-ros-dev:iron --build-arg distro_tag=iron .
+START docker push kaiaai/kaia-ros-dev:iron
