7f088a69da26a2b5ac5c34af0669073c7ae798e8
Ilia O. <iliao@kaia.ai>
2023-09-21 13:54:21 -0700

    Build min

---
+++ utils/build_min.cmd
@@ -0,0 +1,5 @@
+docker login -u kaiaai
+cd .\kaiaai-ros-dev
+docker image rm kaiaai/kaiaai-ros-dev:humble
+docker build --no-cache -t kaiaai/kaiaai-ros-dev:humble --build-arg distro_tag=humble .
+docker push kaiaai/kaiaai-ros-dev:humble
