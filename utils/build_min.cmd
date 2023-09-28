1a58422c15ed47249f56fa9ed3b7a7ca3878b414
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-09-28 00:45:45 -0700

    Update build_min.cmd

--- utils/build_min.cmd
+++ utils/build_min.cmd
@@ -1,4 +1,4 @@
-docker login -u kaiaai
+docker login
 cd .\kaiaai-ros-dev
 docker image rm kaiaai/kaiaai-ros-dev:humble
 docker build --no-cache -t kaiaai/kaiaai-ros-dev:humble --build-arg distro_tag=humble .
