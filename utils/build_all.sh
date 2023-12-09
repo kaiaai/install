e6718ecfbb5975ef9795e8b7a86b065194ea988e
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2023-12-08 19:40:44 -0800

    Update build_all.sh

--- utils/build_all.sh
+++ utils/build_all.sh
@@ -12,21 +12,21 @@ sudo docker image rm kaiaai/kaiaai-ros-dev:iron
 sudo docker system prune -f -a
 cd ./kaiaai-ros
 sudo docker build --no-cache -t kaiaai/kaiaai-ros:humble --build-arg distro_tag=humble .
-sudo docker push kaiaai/kaiaai-ros:humble > push_log_humble.txt &
+sudo docker push kaiaai/kaiaai-ros:humble > push_humble.log &
 sudo docker build --no-cache -t kaiaai/kaiaai-ros:iron --build-arg distro_tag=iron .
-sudo docker push kaiaai/kaiaai-ros:iron > push_log_iron.txt &
+sudo docker push kaiaai/kaiaai-ros:iron > push_iron.log &
 cd ../etc/micro-ros-base-desktop
 sudo docker build --no-cache -t kaiaai/micro-ros-base-desktop:humble --build-arg distro_tag=humble .
-sudo docker push kaiaai/micro-ros-base-desktop:humble > push_log_humble.txt &
+sudo docker push kaiaai/micro-ros-base-desktop:humble > push_humble.log &
 sudo docker build --no-cache -t kaiaai/micro-ros-base-desktop:iron --build-arg distro_tag=iron .
-sudo docker push kaiaai/micro-ros-base-desktop:iron > push_log_iron.txt &
+sudo docker push kaiaai/micro-ros-base-desktop:iron > push_iron.log &
 cd ../micro-ros-agent-desktop
 sudo docker build --no-cache -t kaiaai/micro-ros-agent-desktop:humble --build-arg distro_tag=humble .
-sudo docker push kaiaai/micro-ros-agent-desktop:humble > push_log_humble.txt &
+sudo docker push kaiaai/micro-ros-agent-desktop:humble > push_humble.log &
 sudo docker build --no-cache -t kaiaai/micro-ros-agent-desktop:iron --build-arg distro_tag=iron .
-sudo docker push kaiaai/micro-ros-agent-desktop:iron > push_log_iron.txt &
+sudo docker push kaiaai/micro-ros-agent-desktop:iron > push_iron.log &
 cd ../../kaiaai-ros-dev
 sudo docker build --no-cache -t kaiaai/kaiaai-ros-dev:humble --build-arg distro_tag=humble .
-sudo docker push kaiaai/kaiaai-ros-dev:humble > push_log_humble.txt &
+sudo docker push kaiaai/kaiaai-ros-dev:humble > push_humble.log &
 sudo docker build --no-cache -t kaiaai/kaiaai-ros-dev:iron --build-arg distro_tag=iron .
-sudo docker push kaiaai/kaiaai-ros-dev:iron > push_log_iron.txt &
+sudo docker push kaiaai/kaiaai-ros-dev:iron > push_iron.log &
