80e02ca01e9c5f23ceb5ca5f6e27e0e1c165d930
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2024-03-15 16:46:30 -0700

    Update tag_dev.sh

--- utils/tag_dev.sh
+++ utils/tag_dev.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker tag kaiaai/kaiaai:humble kaiaai/kaiaai-ros-dev:humble
+sudo docker tag kaiaai/kaiaai:humble kaiaai/kaiaai-ros-dev:humble
 sudo docker push kaiaai/kaiaai-ros-dev:humble
-docker tag kaiaai/kaiaai:iron kaiaai/kaiaai-ros-dev:iron
+sudo docker tag kaiaai/kaiaai:iron kaiaai/kaiaai-ros-dev:iron
 sudo docker push kaiaai/kaiaai-ros-dev:iron
