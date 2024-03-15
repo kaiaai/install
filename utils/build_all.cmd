4c96d7d513249c64619715c80a3e40b7f4bfa29b
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2024-03-15 16:45:51 -0700

    Update build_all.cmd

--- utils/build_all.cmd
+++ utils/build_all.cmd
@@ -6,7 +6,9 @@ docker image rm kaiaai/kaiaai-ros-dev:iron
 docker system prune -f -a
 cd .\kaiaai
 docker build --no-cache -t kaiaai/kaiaai:humble --build-arg distro_tag=humble .
-wt --window 0 -d . powershell "docker push kaiaai/kaiaai:humble"
+wt --window 0 -d . powershell "docker push kaiaai/kaiaai:humble; docker tag kaiaai/kaiaai:humble kaiaai/kaiaai-ros-dev:humble; docker push kaiaai/kaiaai-ros-dev:humble"
 docker build --no-cache -t kaiaai/kaiaai:iron --build-arg distro_tag=iron .
 docker push kaiaai/kaiaai:iron
+docker tag kaiaai/kaiaai:iron kaiaai/kaiaai-ros-dev:iron
+docker push kaiaai/kaiaai-ros-dev:iron
 cd ..
