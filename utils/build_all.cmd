92d4e50cece473cb934e3836257f514c7ed3757e
Maker's Pet <iliao@makerspet.com>
2024-03-15 00:29:32 -0700

    Push fix

--- utils/build_all.cmd
+++ utils/build_all.cmd
@@ -11,6 +11,6 @@ docker build --no-cache -t kaiaai/kaiaai:iron --build-arg distro_tag=iron .
 wt --window 0 -d . powershell "docker push kaiaai/kaiaai:iron"
 cd ..
 docker tag kaiaai/kaiaai:humble kaiaai/kaiaai-ros-dev:humble
-wt --window 0 -d . powershell "docker push kaiaai/kaiaai-ros-dev:humble"
+docker push kaiaai/kaiaai-ros-dev:humble
 docker tag kaiaai/kaiaai:iron kaiaai/kaiaai-ros-dev:iron
-wt --window 0 -d . powershell "docker push kaiaai/kaiaai-ros-dev:iron"
+docker push kaiaai/kaiaai-ros-dev:iron
