1c1d137f89f362cfd4932e11803691dc24e59eef
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2024-03-15 16:47:32 -0700

    Update build_all.sh

--- utils/build_all.sh
+++ utils/build_all.sh
@@ -8,7 +8,9 @@ sudo docker image rm kaiaai/kaiaai-ros-dev:iron
 sudo docker system prune -f -a
 cd ./kaiaai
 sudo docker build --no-cache -t kaiaai/kaiaai:humble --build-arg distro_tag=humble .
-sudo docker push kaiaai/kaiaai:humble >> push_humble.log &
+sudo docker push kaiaai/kaiaai:humble >> push_humble.log && sudo docker tag kaiaai/kaiaai:humble kaiaai/kaiaai-ros-dev:humble && sudo docker push kaiaai/kaiaai-ros-dev:humble &
 sudo docker build --no-cache -t kaiaai/kaiaai:iron --build-arg distro_tag=iron .
 sudo docker push kaiaai/kaiaai:iron
+sudo docker tag kaiaai/kaiaai:iron kaiaai/kaiaai-ros-dev:iron
+sudo docker push kaiaai/kaiaai-ros-dev:iron
 cd ..
