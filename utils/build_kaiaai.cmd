51836391bd9ced7e15fdcf2d8e65a73772960a9a
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2024-03-15 16:48:08 -0700

    Update build_kaiaai.cmd

--- utils/build_kaiaai.cmd
+++ utils/build_kaiaai.cmd
@@ -5,5 +5,5 @@ docker build --no-cache -t kaiaai/kaiaai:humble --build-arg distro_tag=humble .
 wt --window 0 -d . powershell "docker push kaiaai/kaiaai:humble"
 docker image rm kaiaai/kaiaai:iron
 docker build --no-cache -t kaiaai/kaiaai:iron --build-arg distro_tag=iron .
-wt --window 0 -d . powershell "docker push kaiaai/kaiaai:iron"
+docker push kaiaai/kaiaai:iron
 cd ..
