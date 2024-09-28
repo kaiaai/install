604412b92c71c643abffbecafb5541b4d9d12273
Ilia O. <Ilia O.>
2024-09-27 23:32:31 -0700

    pull on build

--- docker/utils/attest_humble.cmd
+++ docker/utils/attest_humble.cmd
@@ -1,7 +1,7 @@
 docker login
 docker image rm kaiaai/kaiaai:humble
 docker system prune -f -a
-docker buildx rm container
-docker buildx create --name container --driver=docker-container
+docker buildx create --name kaiaai --driver=docker-container
 cd .\kaiaai
-docker buildx build --builder=container --no-cache --provenance=true -t kaiaai/kaiaai:humble --build-arg distro_tag=humble --push .
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:humble --build-arg distro_tag=humble --push .
+docker pull kaiaai/kaiaai:humble
