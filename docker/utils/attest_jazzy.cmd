f4179d07c51d7a607f640e358c028ed4b61d3dad
Ilia O. <Ilia O.>
2024-12-10 01:46:47 -0800

    Jazzy WiP

---
+++ docker/utils/attest_jazzy.cmd
@@ -0,0 +1,7 @@
+docker login
+docker image rm kaiaai/kaiaai:jazzy
+docker system prune -f -a
+docker buildx create --name kaiaai --driver=docker-container
+cd .\kaiaai
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy --build-arg distro_tag=jazzy --push .
+docker pull kaiaai/kaiaai:jazzy
