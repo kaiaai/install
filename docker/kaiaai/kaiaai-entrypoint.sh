f0a06324163d72bdbf4c2eed8f2bb292d6aad5e1
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2024-12-18 01:36:35 -0800

    Color hello

--- docker/kaiaai/kaiaai-entrypoint.sh
+++ docker/kaiaai/kaiaai-entrypoint.sh
@@ -17,6 +17,6 @@ if [ "$1" = "launch" ]; then
 # ros2 run micro_ros_agent micro_ros_agent udp4 -p 8888
 else
   set -e
-  echo "Visit https://github.com/kaiaai/kaiaai for help"
+  echo "\033[31mVisit https://github.com/kaiaai/kaiaai for help\033[0m"
   exec "$@"
 fi
