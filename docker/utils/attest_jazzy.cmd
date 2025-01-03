d834a896f0174cde24b19e2abe125d459653a97c
Ilia O. <Ilia O.>
2025-01-02 20:30:26 -0800

    Fix buildx cleanup

--- docker/utils/attest_jazzy.cmd
+++ docker/utils/attest_jazzy.cmd
@@ -4,5 +4,5 @@ docker system prune -f -a
 docker buildx create --name kaiaai --driver=docker-container
 cd .\kaiaai
 docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:jazzy --build-arg distro_tag=jazzy --push .
-docker container rm kaiaai --force
+docker rm $(docker ps -a -q) --force
 docker pull kaiaai/kaiaai:jazzy
