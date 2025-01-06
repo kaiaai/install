fe8ef5d3353ed339880437432c825b51cfea2cc8
Ilia O. <Ilia O.>
2025-01-05 20:14:49 -0800

    Attest humble+iron

---
+++ docker/utils/attest_humble_iron.cmd
@@ -0,0 +1,11 @@
+docker login
+docker image rm kaiaai/kaiaai:humble
+docker image rm kaiaai/kaiaai:iron
+docker system prune -f -a
+docker buildx create --name kaiaai --driver=docker-container
+cd .\kaiaai
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:humble --build-arg distro_tag=humble --push .
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:iron --build-arg distro_tag=iron --push .
+docker rm buildx_buildkit_kaiaai0 --force
+docker pull kaiaai/kaiaai:humble
+docker pull kaiaai/kaiaai:iron
