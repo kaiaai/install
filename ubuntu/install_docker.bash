bd5c6aa8675beb158a891bdcafc7e4b683788a45
kaia.ai <33589365+kaiaai@users.noreply.github.com>
2025-03-18 02:52:36 -0700

    Update install_docker.bash

--- ubuntu/install_docker.bash
+++ ubuntu/install_docker.bash
@@ -13,3 +13,5 @@ echo \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo docker run hello-world
