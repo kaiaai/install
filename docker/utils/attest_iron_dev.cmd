71581eeca27799132dc5c997aaac955a36a7575b
Ilia O. <Ilia O.>
2025-11-18 04:07:41 -0800

    iron-dev

---
+++ docker/utils/attest_iron_dev.cmd
@@ -0,0 +1,8 @@
+docker login
+docker image rm kaiaai/kaiaai:iron-dev
+docker system prune -f -a
+docker buildx create --name kaiaai --driver=docker-container
+cd .\kaiaai
+docker buildx build --builder=kaiaai --no-cache --provenance=true -t kaiaai/kaiaai:iron-dev --build-arg distro_tag=iron --push .
+docker rm buildx_buildkit_kaiaai0 --force
+docker pull kaiaai/kaiaai:iron-dev
